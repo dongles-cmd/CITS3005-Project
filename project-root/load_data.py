cafee30fca818cac5ac687336127ce7a04a61c01
LeweiXu <77490452+LeweiXu@users.noreply.github.com>
2024-10-12 10:33:31 +0800

    reasoning

--- project-root/load_data.py
+++ project-root/load_data.py
@@ -13,69 +13,72 @@ existing_items = {}
 existing_parts = {}
 existing_images = {}
 
-# Use unique identifier perferrably URL for URI, then set attribute name for pretty print name
-for procedure in data:
-    # Process procedure
-    procedure_instance = onto.Procedure(procedure['Url']) # Use URL as unique identifier
-    procedure_instance.has_name.append(procedure['Title'])
+with onto:
+    # Use unique identifier perferrably URL for URI, then set attribute name for pretty print name
+    for procedure in data:
+        # Process procedure
+        procedure_instance = onto.Procedure(procedure['Url']) # Use URL as unique identifier
+        procedure_instance.has_name.append(procedure['Title'])
 
-    # Process item of procedure
-    item_name = procedure['Category']
-    if item_name not in existing_items:
-        item_instance = onto.Item(item_name.replace(' ', '_')) # prevent serialization error by removing spaces
-        item_instance.has_name.append(item_name)
-        existing_items[item_name] = item_instance
-    else:
-        item_instance = existing_items[item_name]
-    procedure_instance.procedure_for.append(item_instance)
-    
-    # Process part
-    part_name = procedure['Subject']
-    if part_name not in existing_parts:
-        part_instance = onto.Part(part_name.replace(' ', '_')) # prevent serialization error by removing spaces
-        part_instance.has_name.append(part_name)
-        existing_parts[part_name] = part_instance
-    else:
-        part_instance = existing_parts[part_name]
-    part_instance.part_of.append(item_instance)
-
-    # Process toolbox
-    for tool in procedure['Toolbox']:
-        tool_id = tool['Url']
-        tool_name = tool['Name']
-        if tool_id not in existing_tools:
-            tool_instance = onto.Tool(tool_id) # initialize instance
-            tool_instance.has_name.append(tool_name) # set name attribute
-            existing_tools[tool_id] = tool_instance
-            tool_name_url[tool_name] = tool_id
+        # Process item of procedure
+        item_name = procedure['Category']
+        if item_name not in existing_items:
+            item_instance = onto.Item(item_name.replace(' ', '_')) # prevent serialization error by removing spaces
+            item_instance.has_name.append(item_name)
+            existing_items[item_name] = item_instance
         else:
-            tool_instance = existing_tools[tool_id]
-        procedure_instance.uses_tool.append(tool_instance)
-
-    # Process steps
-    for step in procedure['Steps']:
-        stepid = str(step['StepId'])
-        step_instance = onto.Step(stepid)
-        step_instance.has_order.append(step['Order'])
-        step_instance.has_text.append(step['Text_raw'])
-        procedure_instance.has_step.append(step_instance)
-
-        # Process images in step
-        for url in step['Images']:
-            if url not in existing_images:
-                image_instance = onto.Image(url)
-                existing_images[url] = image_instance
-            else:
-                image_instance = existing_images[url]
-            step_instance.has_image.append(image_instance)
+            item_instance = existing_items[item_name]
+        procedure_instance.procedure_for.append(item_instance)
         
-        # Process tools in step:
-        for tool in step['Tools_extracted']:
-            if tool == 'NA': break
-            if tool not in tool_name_url: continue # not sure what to do here, occurs when tool in toolbox but not in step
-            tool_id = tool_name_url[tool]
-            tool_instance = existing_tools[tool_id] # Should already be defined
-            step_instance.uses_tool.append(tool_instance)
+        # Process part
+        part_name = procedure['Subject']
+        if part_name not in existing_parts:
+            part_instance = onto.Part(part_name.replace(' ', '_')) # prevent serialization error by removing spaces
+            part_instance.has_name.append(part_name)
+            existing_parts[part_name] = part_instance
+        else:
+            part_instance = existing_parts[part_name]
+        part_instance.part_of.append(item_instance)
+
+        # Process toolbox
+        for tool in procedure['Toolbox']:
+            tool_id = tool['Url']
+            tool_name = tool['Name']
+            if tool_id not in existing_tools:
+                tool_instance = onto.Tool(tool_id) # initialize instance
+                tool_instance.has_name.append(tool_name) # set name attribute
+                existing_tools[tool_id] = tool_instance
+                tool_name_url[tool_name] = tool_id
+            else:
+                tool_instance = existing_tools[tool_id]
+            procedure_instance.uses_tool.append(tool_instance)
+
+        # Process steps
+        for step in procedure['Steps']:
+            stepid = str(step['StepId'])
+            step_instance = onto.Step(stepid)
+            step_instance.has_order.append(step['Order'])
+            step_instance.has_text.append(step['Text_raw'])
+            procedure_instance.has_step.append(step_instance)
+
+            # Process images in step
+            for url in step['Images']:
+                if url not in existing_images:
+                    image_instance = onto.Image(url)
+                    existing_images[url] = image_instance
+                else:
+                    image_instance = existing_images[url]
+                step_instance.has_image.append(image_instance)
+            
+            # Process tools in step:
+            for tool in step['Tools_extracted']:
+                if tool == 'NA': break
+                if tool not in tool_name_url: continue # not sure what to do here, occurs when tool in toolbox but not in step
+                tool_id = tool_name_url[tool]
+                tool_instance = existing_tools[tool_id] # Should already be defined
+                step_instance.uses_tool.append(tool_instance)
+
+    sync_reasoner_hermit()
 
 onto.save(file = "ifixit_knowledge_graph.rdf", format='rdfxml')
 
