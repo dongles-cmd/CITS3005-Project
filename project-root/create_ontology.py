c10846e870727d0d5a17a6be4494c6f7be0f6ef3
LeweiXu <77490452+LeweiXu@users.noreply.github.com>
2024-10-12 11:43:19 +0800

    AAAAAAAAA

--- project-root/create_ontology.py
+++ project-root/create_ontology.py
@@ -1,55 +1,86 @@
 from owlready2 import *
- 
+
 # Load an empty ontology
 onto = get_ontology("http://test.org/ifixit.com#")
- 
+
 with onto:
     # Define classes
     class Procedure(Thing): pass
     class Item(Thing): pass
-    class Part(Item): pass # Part is a subclass of Item
+    class Part(Item): pass  # Part is a subclass of Item
     class Tool(Thing): pass
     class Step(Thing): pass
     class Image(Thing): pass
 
-    class has_name(DataProperty):
-        domain = [Procedure, Item, Part, Tool] 
-        range = [str] 
+    # Define data properties
+    class procedure_has_name(DataProperty):
+        domain = [Procedure]
+        range = [str]
+
+    class item_has_name(DataProperty):
+        domain = [Item]
+        range = [str]
+
+    class part_has_name(DataProperty):
+        domain = [Part]
+        range = [str]
+
+    class tool_has_name(DataProperty):
+        domain = [Tool]
+        range = [str]
+
     class has_order(DataProperty):
         domain = [Step]
         range = [int]
+
     class has_text(DataProperty):
         domain = [Step]
         range = [str]
 
-    # Define relationships
-    class uses_tool(ObjectProperty):
-        domain = [Procedure, Step]
+    # Define object properties
+    class procedure_uses_tool(ObjectProperty):
+        domain = [Procedure]
         range = [Tool]
+
+    class step_uses_tool(ObjectProperty):
+        domain = [Step]
+        range = [Tool]
+
     class has_step(ObjectProperty):
         domain = [Procedure]
         range = [Step]
+
     class has_image(ObjectProperty):
         domain = [Step]
         range = [Image]
-    class sub_procedure_for(ObjectProperty):
+
+    class sub_procedure_of(ObjectProperty):
         domain = [Procedure]
         range = [Procedure]
+        transitive = True
+
     class procedure_for(ObjectProperty):
         domain = [Procedure]
         range = [Item]
 
-    # An item, with a subclass relation that is transitive
-    # and a part-of relation that identifies when one item is a part of another item
     class part_of(ObjectProperty): 
-        domain = [Item] # Will allow Part to be used as it is a subclass of Item
+        domain = [Item]  # Will allow Part to be used as it is a subclass of Item
         range = [Item]
         transitive = True
-    # Tools used in a step of the procedure must appear in the toolbox of the procedure
-    class Procedure(Thing):
-        equivalent_to = [Thing & has_step.some(Step) & uses_tool.some(Tool)]
-    # A sub-procedure of a procedure must be a procedure for the same item or a part of that item
-    class Procedure(Thing):
-        equivalent_to = [Thing & sub_procedure_for.only(procedure_for.some(Item) | procedure_for.some(part_of))]
 
-onto.save(file = "ifixit_ontology.owl", format='rdfxml')
+    class Procedure(Thing):
+        equivalent_to = [
+            has_step.some(Step) &
+            procedure_uses_tool.some(Tool) &
+            has_step.some(step_uses_tool.some(Tool))
+        ]
+
+    class Procedure(Thing):
+        equivalent_to = [
+            has_step.some(Step) & 
+            procedure_for.some(Item) & 
+            sub_procedure_of.only(procedure_for.some(Item))  # Sub-procedures share the same item
+        ]
+
+# Save the ontology
+onto.save(file="ifixit_ontology.owl", format='rdfxml')
--- project-root/load_data.py
+++ project-root/load_data.py
@@ -1,10 +1,11 @@
 from owlready2 import *
 import json
 
+# Load your ontology
 onto = get_ontology('ifixit_ontology.owl').load()
 
-# Use small test json file first 
-with open('test.json', 'r') as file:
+# Use small test JSON file first
+with open('test_1_proc.json', 'r') as file:
     data = json.load(file)
 
 tool_name_url = {}
@@ -14,17 +15,17 @@ existing_parts = {}
 existing_images = {}
 
 with onto:
-    # Use unique identifier perferrably URL for URI, then set attribute name for pretty print name
+    # Use unique identifier (preferably URL for URI), then set the attribute name for pretty print name
     for procedure in data:
         # Process procedure
-        procedure_instance = onto.Procedure(procedure['Url']) # Use URL as unique identifier
-        procedure_instance.has_name.append(procedure['Title'])
+        procedure_instance = onto.Procedure(procedure['Url'])  # Use URL as unique identifier
+        procedure_instance.procedure_has_name.append(procedure['Title'])
 
         # Process item of procedure
         item_name = procedure['Category']
         if item_name not in existing_items:
-            item_instance = onto.Item(item_name.replace(' ', '_')) # prevent serialization error by removing spaces
-            item_instance.has_name.append(item_name)
+            item_instance = onto.Item(item_name.replace(' ', '_'))  # Prevent serialization error by removing spaces
+            item_instance.item_has_name.append(item_name)
             existing_items[item_name] = item_instance
         else:
             item_instance = existing_items[item_name]
@@ -33,8 +34,8 @@ with onto:
         # Process part
         part_name = procedure['Subject']
         if part_name not in existing_parts:
-            part_instance = onto.Part(part_name.replace(' ', '_')) # prevent serialization error by removing spaces
-            part_instance.has_name.append(part_name)
+            part_instance = onto.Part(part_name.replace(' ', '_'))  # Prevent serialization error by removing spaces
+            part_instance.part_has_name.append(part_name)
             existing_parts[part_name] = part_instance
         else:
             part_instance = existing_parts[part_name]
@@ -45,20 +46,23 @@ with onto:
             tool_id = tool['Url']
             tool_name = tool['Name']
             if tool_id not in existing_tools:
-                tool_instance = onto.Tool(tool_id) # initialize instance
-                tool_instance.has_name.append(tool_name) # set name attribute
+                tool_instance = onto.Tool(tool_id)  # Initialize instance
+                tool_instance.tool_has_name.append(tool_name)  # Set name attribute
                 existing_tools[tool_id] = tool_instance
                 tool_name_url[tool_name] = tool_id
             else:
                 tool_instance = existing_tools[tool_id]
-            procedure_instance.uses_tool.append(tool_instance)
+            procedure_instance.procedure_uses_tool.append(tool_instance)
 
         # Process steps
         for step in procedure['Steps']:
             stepid = str(step['StepId'])
             step_instance = onto.Step(stepid)
-            step_instance.has_order.append(step['Order'])
-            step_instance.has_text.append(step['Text_raw'])
+            if step['Order'] not in step_instance.has_order:
+                step_instance.has_order.append(step['Order'])
+            
+            if step['Text_raw'] not in step_instance.has_text:
+                step_instance.has_text.append(step['Text_raw'])
             procedure_instance.has_step.append(step_instance)
 
             # Process images in step
@@ -70,17 +74,19 @@ with onto:
                     image_instance = existing_images[url]
                 step_instance.has_image.append(image_instance)
             
-            # Process tools in step:
+            # Process tools in step
             for tool in step['Tools_extracted']:
                 if tool == 'NA': break
-                if tool not in tool_name_url: continue # not sure what to do here, occurs when tool in toolbox but not in step
+                if tool not in tool_name_url: continue  # Not sure what to do here, occurs when tool is in toolbox but not in step
                 tool_id = tool_name_url[tool]
-                tool_instance = existing_tools[tool_id] # Should already be defined
-                step_instance.uses_tool.append(tool_instance)
+                tool_instance = existing_tools[tool_id]  # Should already be defined
+                step_instance.step_uses_tool.append(tool_instance)
 
-    sync_reasoner_hermit()
+    # Sync the reasoner
+    sync_reasoner(infer_property_values=True)
 
-onto.save(file = "ifixit_knowledge_graph.rdf", format='rdfxml')
+# Save the updated ontology
+onto.save(file="ifixit_knowledge_graph.rdf", format='rdfxml')
 
 # # Function to output relationships of Procedures and their Steps
 # def print_procedure_relationships():
@@ -93,7 +99,7 @@ onto.save(file = "ifixit_knowledge_graph.rdf", format='rdfxml')
 
 #         # Print related Steps
 #         for step in procedure.has_step:
-#             print(f"  Step: {step}, Order: {step.order}, Text: {step.text}")
+#             print(f"  Step: {step}, Order: {step.has_order}, Text: {step.has_text}")
             
 #             # Print related Tools in the step
 #             for tool in step.step_uses_tool:
