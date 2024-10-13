9a30a214e1993e05c8852ae8ae518ec9ce5b34da
Marc Labouchardiere <marc.labouchardiere13@gmail.com>
2024-10-13 17:51:54 +0800

    push

--- project-root/graph/sparql_queries.py
+++ project-root/graph/sparql_queries.py
@@ -2,7 +2,8 @@
 # NOTE: Run `python3 -m graph.sparql_queries` in /project-root
 
 from rdflib import Graph
-from config import KNOWLEDGE_GRAPH, ONTOLOGY
+from config import KNOWLEDGE_GRAPH
+from owlready2 import get_ontology
 
 # Function to output relationships of Procedures and their Steps
 def print_procedure_relationships(onto):
@@ -18,83 +19,89 @@ def print_procedure_relationships(onto):
             print(f"\tStep: {step}, Order: {step.has_order}, Text: {step.has_text}")
             
             # Print related Tools in the step
-            for tool in step.step_uses_tool:
+            for tool in step.uses_tool:
                 print(f"\t\tUses Tool: {tool}")
 
         # Print related Tools in the procedure toolbox
-        for tool in procedure.procedure_uses_tool:
+        for tool in procedure.uses_tool:
             print(f"\tUses Toolbox Tool: {tool}")
 
 # Call the function to print relationships
-print_procedure_relationships(ONTOLOGY)
+onto = get_ontology(KNOWLEDGE_GRAPH).load()
+print_procedure_relationships(onto)
 
-# Load in the knowledge graph from RDF file
-g = Graph().parse(KNOWLEDGE_GRAPH)
+# # Load in the knowledge graph from RDF file
+# g = Graph()
+# g.parse(KNOWLEDGE_GRAPH)
 
-# Get the names of procedures with more than 12 steps
-query = """
-    PREFIX ex: <http://test.org/ifixit.com#>
+# # Get the names of procedures with more than 12 steps
+# query = """
+#     PREFIX ex: <http://test.org/ifixit.com#>
 
-    SELECT ?procedure_name (COUNT(?step) AS ?step_count)
-    WHERE {
-        ?procedure ex:has_step ?step ;
-            ex:has_name ?procedure_name .
-    }
-    GROUP BY ?procedure
-    HAVING (COUNT(?step) > 12)
-"""
-print("Procedures with more than 12 steps:")
-for result in g.query(query):
-    print(f"Procedure [{result['procedure_name']}], number of steps [{result['step_count']}]")
+#     SELECT ?procedure_name (COUNT(?step) AS ?step_count)
+#     WHERE {
+#         ?procedure ex:has_step ?step ;
+#             ex:has_name ?procedure_name .
+#     }
+#     GROUP BY ?procedure
+#     HAVING (COUNT(?step) > 12)
+# """
+# print("Procedures with more than 12 steps:")
+# for result in g.query(query):
+#     print(f"Procedure [{result['procedure_name']}], number of steps [{result['step_count']}]")
 
-# Get names of items with more than 10 procedures written for them
-query = """
-    PREFIX ex: <http://test.org/ifixit.com#>
+# # Get names of items with more than 10 procedures written for them
+# query = """
+#     PREFIX ex: <http://test.org/ifixit.com#>
 
-    SELECT ?item_name (COUNT(?procedure) as ?procedure_count)
-    WHERE {
-        ?procedure ex:procedure_for ?item .
-        ?item ex:has_name ?item_name .
-    }
-    GROUP BY ?item
-    HAVING (COUNT(?procedure) > 10)
-"""
-print("\nItems with more than 10 procedures:")
-for result in g.query(query):
-    print(f"Item [{result['item_name']}], number of procedures [{result['procedure_count']}]")
+#     SELECT ?item_name (COUNT(?procedure) as ?procedure_count)
+#     WHERE {
+#         ?procedure ex:procedure_for ?item .
+#         ?item ex:has_name ?item_name .
+#     }
+#     GROUP BY ?item
+#     HAVING (COUNT(?procedure) > 10)
+# """
+# print("\nItems with more than 10 procedures:")
+# for result in g.query(query):
+#     print(f"Item [{result['item_name']}], number of procedures [{result['procedure_count']}]")
 
-# Find procedures where tools mentioned in toolbox don't appear in any steps
-query = """
-    PREFIX ex: <http://test.org/ifixit.com#>
+# # Find procedures where tools mentioned in toolbox don't appear in any steps
+# query = """
+#     PREFIX ex: <http://test.org/ifixit.com#>
 
-    SELECT ?procedure_name ?tool_name
-    WHERE {
-        ?procedure ex:uses_tool ?tool ;
-                ex:has_name ?procedure_name .
-        ?tool ex:has_name ?tool_name .
-        FILTER NOT EXISTS {
-            ?procedure ex:has_step ?step .
-            ?step ex:uses_tool ?tool .
-        }
-    }
-"""
-print("\nProcedures with tools in toolbox in mentioned in any step:")
-for result in g.query(query):
-    print(f"Procedure [{result['procedure_name']}] has tool [{result['tool_name']}] in toolbox but not in any steps")
+#     SELECT ?procedure_name ?tool_name
+#     WHERE {
+#         ?procedure ex:uses_tool ?tool ;
+#                 ex:has_name ?procedure_name .
+#         ?tool ex:has_name ?tool_name .
+#         FILTER NOT EXISTS {
+#             ?procedure ex:has_step ?step .
+#             ?step ex:uses_tool ?tool .
+#         }
+#     }
+# """
+# print("\nProcedures with tools in toolbox in mentioned in any step:")
+# for result in g.query(query):
+#     print(f"Procedure [{result['procedure_name']}] has tool [{result['tool_name']}] in toolbox but not in any steps")
 
-# Find potentially dangerous procedures 
-query = """
-    PREFIX ex: <http://test.org/ifixit.com#>
+# # Find potentially dangerous procedures 
+# # Ensure superstring match not included (e.g. "carefully")
+# query = """
+#     PREFIX ex: <http://test.org/ifixit.com#>
 
-    SELECT ?procedure_name
-    WHERE {
-        ?procedure ex:has_step ?step ;
-            ex:has_name ?procedure_name .
-        ?step ex:has_text ?text .
-        FILTER (CONTAINS(LCASE(?text), "careful") || CONTAINS(LCASE(?text), "dangerous"))
-    }
-"""
+#     SELECT ?procedure_name
+#     WHERE {
+#         ?procedure ex:has_step ?step ;
+#             ex:has_name ?procedure_name .
+#         ?step ex:has_text ?text .
+#         FILTER (
+#             REGEX(STR(?text), "careful", "i") || 
+#             REGEX(STR(?text), "dangerous", "i")
+#         )
+#     }
+# """
 
-print("\nPotentially hazardous procedures:")
-for result in g.query(query):
-    print(f"Procedures [{result['procedure_name']}] may be potentially hazardous")
+# print("\nPotentially hazardous procedures:")
+# for result in g.query(query):
+#     print(f"Procedures [{result['procedure_name']}] may be potentially hazardous")
